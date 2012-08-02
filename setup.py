a9957c4adee383c7730da6c8c3e7897016c5a7e1
Peter Kuma <peterkuma@centrum.sk>
2012-08-02 10:09:01 +0200

    Bulk commit: ccplot 1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ module1 = Extension('cctk',
                     sources=['cctkmodule.c'])
 
 setup(name='ccplot',
-      version='1.1',
+      version='1.2',
       description='CloudSat/CALIPSO data processing helper routines',
       author='Peter Kuma',
       author_email='peterkuma@waveland.org',
