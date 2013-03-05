c3247db6c78ce6a6d2b1a636b698737ca2d77a7d
Peter Kuma <peterkuma@waveland.org>
2013-03-05 09:02:32 +0100

    ccplot 1.5-rc3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy
 
 setup(
     name='ccplot',
-    version='1.5-rc2',
+    version='1.5-rc3',
     description='CloudSat and CALIPSO plotting tool',
     long_description="""
     ccplot is a command-line application that reads CloudSat, CALISO
