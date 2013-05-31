5989b79aba5f4ebff644ef20f53200c877b6a777
Peter Kuma <peterkuma@waveland.org>
2013-05-31 09:41:31 +0200

    ccplot v1.4.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ module1 = Extension('cctk',
                     sources=['cctkmodule.c'])
 
 setup(name='ccplot',
-      version='1.4.4',
+      version='1.4.5',
       description='CloudSat and CALIPSO plotting tool',
       long_description="""
       ccplot is a command-line application that reads CloudSat, CALISO
