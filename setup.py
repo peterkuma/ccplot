a7a238d1b469dec62c64e1f735a9713a811a16d9
Peter Kuma <peter@peterkuma.net>
2023-09-08 18:45:20 +0100

    v2.1.4

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if sys.platform == 'darwin':
 
 setup(
     name='ccplot',
-    version='2.1.3',
+    version='2.1.4',
     description='CloudSat and CALIPSO plotting tool',
     long_description="""ccplot is an open source command-line program for
     plotting profile, layer and earth view data sets from CloudSat, CALIPSO
